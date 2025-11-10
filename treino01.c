8d311660501fcbe02a9c8ad4e5a17c2e88471509
Andre Filipe <andrecian2003@gmail.com>
2025-11-10 11:55:44 -0300

    atividade 1

--- treino01.c
+++ treino01.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main(){
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    //1) Faça um programa que leia um número inteiro e o imprima.
+    int n1 = 0;
+    printf("Digite um numero: ");
+    scanf("%d",&n1);
+    printf("O numero digtado foi %d",n1);
+     
+    getchar();
+    getchar();
     return 0;
 }
